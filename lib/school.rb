64a4b41a3305d2ad5870120a750a20f4b75e2774
Dakota Coppage <dakotakcoppage@me.com>
2020-08-13 07:36:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,13 +5,6 @@ class School
     @name = name
     @roster = {}
   end
-  
-  def sort
-    nu_hash = {}
-    roster.each.do |a,b|
-      nu_hash[a] = b.sort
-    end
-    nu_hash
-  end
+
   
 end
